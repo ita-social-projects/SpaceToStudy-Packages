869460bd56bc8e2f2d23a0aa5f4aeab786e3a4d4
Iarynovskyi <dimayar20061@gmail.com>
2025-04-24 16:09:43 +0300

    do build

--- packages/components/types/components/lib/MenuItem/MenuItem.d.ts
+++ packages/components/types/components/lib/MenuItem/MenuItem.d.ts
@@ -1,14 +1,14 @@
-import React  from 'react';
-import { MenuItemVariant } from "./MenuItem.constants";
+import React from 'react';
+import { MenuItemProps as CommonMenuItemProps } from './MenuItem.types';
+import { MenuItemVariant } from './MenuItem.constants';
 import './MenuItem.scss';
-
-import {MenuItemProps as CommonMenuItemProps} from "./MenuItem.types";
 interface MenuItemProps extends CommonMenuItemProps {
-    isDropdown?: boolean
-    density?: 1 | 2
-    isToggled?: boolean
-    onRemove?: () => void
-    variant?: MenuItemVariant
+    isDropdown?: boolean;
+    density?: 1 | 2;
+    isToggled?: boolean;
+    onRemove?: () => void;
+    variant?: MenuItemVariant;
 }
-declare const MenuItem: React.ForwardRefExoticComponent<Omit<MenuItemProps, "ref"> & React.RefAttributes<HTMLElement>>;
+
+declare const MenuItem: React.ForwardRefExoticComponent<MenuItemProps & React.RefAttributes<HTMLLIElement>>;
 export default MenuItem;
