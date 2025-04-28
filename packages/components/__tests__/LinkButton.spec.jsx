367cd147340d5e8e38bdc4e87a0980ca92ca7c23
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-04-28 17:06:22 +0300

    Updated unit tests for LinkButton

--- packages/components/__tests__/LinkButton.spec.jsx
+++ packages/components/__tests__/LinkButton.spec.jsx
@@ -1,16 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import LinkButton from "../lib/LinkButton/LinkButton";
 
-describe("LinkButton Component", () => {
-  const mockText = "Link Button";
-  const mockLink = "/test-link";
-  const props = {
-    variant: "light",
-    size: "md",
-  };
+const mockText = "Link Button";
+const mockLink = "/test-link";
+const props = {
+  variant: "light",
+  size: "md",
+};
 
+describe("LinkButton Component", () => {
   beforeEach(() => {
     render(
       <Router>
@@ -41,5 +42,34 @@ describe("LinkButton Component", () => {
   it("should not have disabled class", () => {
     const linkElement = screen.getByRole("link");
     expect(linkElement).not.toHaveClass("s2s-link-button-disabled");
+  });
+});
+
+describe("LinkButton Component with loading and disabled", () => {
+  beforeEach(() => {
+    render(
+      <Router>
+        <LinkButton
+          size={props.size}
+          to={mockLink}
+          variant={props.variant}
+          loading
+          disabled
+        >
+          {mockText}
+        </LinkButton>
+      </Router>
+    );
+  });
+
+  it("should have loader", () => {
+    const loaderElement = screen.getByTestId("loader");
+    expect(loaderElement).toBeInTheDocument();
+  });
+
+  it("should have disabled class", () => {
+    const linkElement = screen.getByRole("link");
+    expect(linkElement).toHaveClass("s2s-link-button-disabled");
+    expect(linkElement).toHaveAttribute("href", "/");
   });
 });
