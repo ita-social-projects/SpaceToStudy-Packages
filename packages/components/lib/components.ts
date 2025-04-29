2338c1b8c06be61bc53d402e20c28ab32e849ba6
Iarynovskyi <dimayar20061@gmail.com>
2025-04-29 18:53:08 +0300

    problem with color and bold text..

--- packages/components/lib/components.ts
+++ packages/components/lib/components.ts
@@ -3,5 +3,9 @@ import Alert from "./Alert/Alert";
 import IconButton from "./IconButton/IconButton"
 import UserAvatar from "./UserAvatar/UserAvatar";
 import MenuItem from "./MenuItem/MenuItem";
-import { MenuItemColorVariant, MenuItemVariant } from "./MenuItem/MenuItem.constants";
-export { Button, Alert, IconButton, UserAvatar, MenuItem, MenuItemColorVariant, MenuItemVariant };
+import {MenuItemColorVariant} from "./MenuItem/MenuItem.constants";
+import {MenuItemVariant} from "./MenuItem/MenuItem.constants";
+import {MenuItemProps} from "./MenuItem/MenuItem.types";
+import {OnItemClickArgs} from "./MenuItem/MenuItem.types"
+export { Button, Alert, IconButton, UserAvatar, MenuItem, MenuItemColorVariant, MenuItemVariant, MenuItemProps, OnItemClickArgs };
+
