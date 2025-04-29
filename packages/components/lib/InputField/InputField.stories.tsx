fdc2c65c30f78cfaf0f100b2c2b7093cbff39dcc
Ulia <ulianaescha05@gmail.com>
2025-04-29 12:28:33 +0300

    Add react declaration

--- packages/components/lib/InputField/InputField.stories.tsx
+++ packages/components/lib/InputField/InputField.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { InputFieldVariantEnum } from "./InputField.constants";
 import InputField, {
   InputFieldProps,
