7b60a39dc6c39a680b9e89f5a7c0b5c94fbcc8df
Vitaliy Kormylo <65959529+VKormylo@users.noreply.github.com>
2025-05-16 16:30:04 +0300

    Implemented LinkButton component (#29)
    * Added LinkButton component
    
    * Updated unit tests for LinkButton

--- packages/components/lib/components.ts
+++ packages/components/lib/components.ts
@@ -1,14 +1,33 @@
 import Button from "./Button/Button";
 import Alert from "./Alert/Alert";
-import IconButton from "./IconButton/IconButton"
+import LinkButton from "./LinkButton/LinkButton";
+import IconButton from "./IconButton/IconButton";
 import UserAvatar from "./UserAvatar/UserAvatar";
 import MenuItem from "./MenuItem/MenuItem";
 import Menu from "./Menu/Menu";
-import { MenuItemColorVariant, MenuItemVariant } from "./MenuItem/MenuItem.constants";
+import {
+  MenuItemColorVariant,
+  MenuItemVariant,
+} from "./MenuItem/MenuItem.constants";
 import Badge from "./Badge/Badge";
 import Chip from "./Chip/Chip";
 import InputField from "./InputField/InputField";
 import { InputFieldVariantEnum } from "./InputField/InputField.constants";
 import DividerComponent from "./Divider/Divider";
 
-export { Button, Alert, DividerComponent, IconButton, UserAvatar, MenuItem, Menu, MenuItemColorVariant, MenuItemVariant, Badge, Chip, InputField, InputFieldVariantEnum };
+export {
+  Button,
+  Alert,
+  LinkButton,
+  DividerComponent,
+  IconButton,
+  UserAvatar,
+  MenuItem,
+  Menu,
+  MenuItemColorVariant,
+  MenuItemVariant,
+  Badge,
+  Chip,
+  InputField,
+  InputFieldVariantEnum,
+};
